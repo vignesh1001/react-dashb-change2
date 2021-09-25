c6ac9ba2b392c460f0b9106d04cd6e4275f60194
Vignesh <vignesh1001@users.noreply.github.com>
2021-09-25 07:16:15 +0530

    Add files via upload

---
+++ src/pages/Leads/index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+import Grid from "@mui/material/Grid";
+import ContentTable from "./ContentTable";
+import MainCard from "../../components/cards/MainCard";
+import { gridSpacing } from "../../store/constant";
+
+const Leads = () => {
+	return (
+		<MainCard title="Lead Management" darkTitle headerStyle={{color: '#006AB4'}}>
+			<Grid container spacing={gridSpacing}>
+				<ContentTable />
+			</Grid>
+		</MainCard>
+	);
+};
+
+export default Leads;
